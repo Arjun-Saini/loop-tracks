8588a7cb6329cf82c1d2267c235666836de3a53e
Arjun-Saini <arjun.saini@gmail.com>
2021-07-27 15:52:43 -0500

    i2c address algorithm

--- master/src/master.cpp
+++ master/src/master.cpp
@@ -3,24 +3,81 @@
 /******************************************************/
 
 #include "Particle.h"
-#line 1 "e:/IoT/loop-tracks/master/src/master.ino"
+#line 1 "c:/Users/Arjun/Documents/GitHub/loop_tracks/master/src/master.ino"
 void setup();
 void loop();
-#line 1 "e:/IoT/loop-tracks/master/src/master.ino"
+void randomizeAddress();
+#line 1 "c:/Users/Arjun/Documents/GitHub/loop_tracks/master/src/master.ino"
 SYSTEM_MODE(MANUAL)
+
+int slaveCountExpected = 2;
+int slaveCount;
 
 void setup() {
   Serial.begin(9600);
+  delay(2000);
   Wire.begin();
+  randomizeAddress();
 }
 
 void loop() {
-  byte transmission = 1;
-  Wire.beginTransmission(0x20);
-  Wire.write("transmission");
-  Wire.endTransmission();
-
-  Wire.requestFrom(0x20, 7);
-  Serial.println(Wire.available());
   delay(500);
+}
+
+void randomizeAddress(){
+  while(slaveCount != slaveCountExpected){
+    //scan through each i2c address
+    slaveCount = 0;
+    for(int i = 8; i <= 119; i++){
+      Serial.println("\nrequest code 1, address: " + String(i));
+      Wire.beginTransmission(i);
+      Wire.write('1');
+      Wire.endTransmission();
+
+      Wire.requestFrom(i, 24);
+      if(Wire.available() > 0){
+        Serial.println("transmission recieved from: " + String(i));
+
+        slaveCount++;
+
+        String inputBuffer = "";
+        char c;
+        for(int j = 0; j < 24; j++){
+          c = Wire.read();
+          inputBuffer += c;
+        }
+        Wire.beginTransmission(i);
+        Wire.write(inputBuffer);
+        Serial.println("device id: " + inputBuffer);
+        Wire.endTransmission();
+        Serial.println("transmission sent to: " + String(i));
+
+        Wire.beginTransmission(i);
+        Wire.write('2');
+        Wire.endTransmission();
+
+        Serial.println("request code 2, address: " + String(i));
+        Wire.requestFrom(i, 4);
+        inputBuffer = "";
+        for(int j = 0; j < 4; j++){
+          inputBuffer += (char)Wire.read();
+        }
+        
+        Serial.println("conflict verification: " + inputBuffer);
+      }
+    }
+  }
+
+  Serial.println("Connected to: ");
+  for(int i = 8; i<= 119; i++){
+    Wire.beginTransmission(i);
+    Wire.write('1');
+    Wire.endTransmission();
+
+    Wire.requestFrom(i, 24);
+    if(Wire.available() > 0){
+      Serial.print(i);
+      Serial.print(", ");
+    }
+  }
 }
--- slave/src/slave.ino
+++ slave/src/slave.ino
@@ -1,24 +1,85 @@
 SYSTEM_MODE(MANUAL)
 
-int address = 0x20;
+int address;
+int requestMode = 0;
+bool verifyAddress = false;
+String deviceID = System.deviceID();
 
 void setup() {
   Serial.begin(9600);
+  address = random(8, 64);
   Wire.begin(address);
   Wire.onReceive(dataReceived);
   Wire.onRequest(dataRequest);
 }
 
 void loop() {
-  Serial.println(Wire.available());
   delay(100);
 }
 
+char c;
+
 void dataReceived(int count){
-  Serial.println("data received");
+  char inputBuffer[Wire.available()];
+  int counter = 0;
+
+  Serial.println("count: ");
+  Serial.print(Wire.available());
+  Serial.println();
+
+  while(Wire.available() > 0){
+    c = Wire.read();
+    Serial.println(c);
+    inputBuffer[counter] = c;
+    counter++;
+  }
+  Serial.println("data received: ");
+  Serial.print(inputBuffer);
+  Serial.println();
+
+  if(inputBuffer[0] == '1'){
+    requestMode = 1;
+  }else if(inputBuffer[0] == '2'){
+    requestMode = 2;
+  }else{
+    for(int i = 0; i < 24; i++){
+      if(deviceID.charAt(i) != inputBuffer[i]){
+        verifyAddress = false;
+        break;
+      }
+      verifyAddress = true;
+    }
+    if(verifyAddress){
+      Serial.println("correct address");
+    }else{
+      Serial.println("randomize address");
+      address = random(64, 120);
+      Wire.end();
+      Wire.begin(address);
+      Wire.onReceive(dataReceived);
+      Wire.onRequest(dataRequest);
+      requestMode = 0;
+    }
+  }
 }
 
 void dataRequest(){
   Serial.println("request received");
-  Wire.write(address);
+  switch(requestMode){
+    case 1:{
+      Serial.println("request mode 1");
+      Wire.write(deviceID);
+      break;
+    }
+    case 2:{
+      Serial.println("request mode 2");
+      if(verifyAddress){
+        Wire.write("pass");
+        verifyAddress = false;
+      }else{
+        Wire.write("fail");
+      }
+      break;
+    }
+  }
 }
