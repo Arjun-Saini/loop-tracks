45c43df7ae5e669ec50f47bbc0092c7c2d4876df
Arjun-Saini <69866165+Arjun-Saini@users.noreply.github.com>
2022-06-28 12:46:27 -0500

    red line neopixel

--- master/src/master.cpp
+++ master/src/master.cpp
@@ -7,15 +7,17 @@
 #include "HttpClient.h"
 #include "JsonParserGeneratorRK.h"
 #include "math.h"
-#include "dotstar.h"
+#include "neopixel.h"
 
 float getDistance(float trainLat, float trainLon);
 void setup();
 void loop();
 void randomizeAddress();
 hal_i2c_config_t acquireWireBuffer();
-
 #line 6 "/Users/sainihome/Documents/GitHub/loop-tracks/master/src/master.ino"
+#define RED_NUM_PIXELS 40
+
+//checkpoints mark each turn on a line
 class Checkpoint{
   public:
   
@@ -56,10 +58,14 @@ http_header_t headers[] = {
 http_request_t request;
 http_response_t response;
 
-Checkpoint redLineCheckpoints[] = {Checkpoint(41.853028, -87.63109), Checkpoint(41.9041, -87.628921), Checkpoint(41.903888, -87.639506), Checkpoint(41.913732, -87.652380), Checkpoint(41.924615, -87.716979)};
+Checkpoint redLineCheckpoints[] = {Checkpoint(41.853028, -87.63109), Checkpoint(41.9041, -87.628921), Checkpoint(41.903888, -87.639506), Checkpoint(41.913732, -87.652380), Checkpoint(41.9253, -87.65286)};
+int redLineScalers[] = {25, 3, 7, 5}; //values should add up to RED_NUM_PIXELS
+int redLineOutput[RED_NUM_PIXELS];
 
 HttpClient http;
 JsonParserStatic<10000, 1000> parser;
+
+Adafruit_NeoPixel strip(RED_NUM_PIXELS, D2, 0x02);
 
 void setup() {
   Serial.begin(9600);
@@ -76,11 +82,14 @@ void setup() {
 
   acquireWireBuffer();
   Wire.begin();
-  randomizeAddress();
+  //randomizeAddress();
 
   request.hostname = "lapi.transitchicago.com";
   request.port = 80;
   request.path = "/api/1.0/ttpositions.aspx?key=00ff09063caa46748434d5fa321d048f&rt=red&outputType=JSON";
+
+  strip.begin();
+  strip.show();
 }
 
 void loop() {
@@ -98,26 +107,99 @@ void loop() {
   while(true){
     JsonReference train = parser.getReference().key("ctatt").key("route").index(0).key("train").index(count);
     String nextStation = train.key("nextStaNm").valueString();
-    String trainDir = train.key("trDr").valueString();
+    int trainDir = train.key("trDr").valueString().toInt();
     float lat = train.key("lat").valueString().toFloat();
     float lon = train.key("lon").valueString().toFloat();
-    int pos;
 
     if(nextStation.length() <= 1){
       break;
     }
 
-    Serial.printf("Train %i: ", count);
-    int arrSize = sizeof(redLineCheckpoints) / sizeof(redLineCheckpoints[0]);
-    float checkpointDistances[arrSize];
-    for(int i = 0; i < arrSize; i++){
+    //Serial.printf("Train %i: ", count);
+    int checkpointCount = sizeof(redLineCheckpoints) / sizeof(redLineCheckpoints[0]);
+    float checkpointDistances[checkpointCount];
+    for(int i = 0; i < checkpointCount; i++){
       checkpointDistances[i] = redLineCheckpoints[i].getDistance(lat, lon);
     }
-    float* closestCheckpoint = std::min_element(checkpointDistances, checkpointDistances + arrSize);
-    Serial.println(closestCheckpoint - checkpointDistances);
+    float* closestCheckpoint = std::min_element(checkpointDistances, checkpointDistances + checkpointCount);
+    int closestIndex = closestCheckpoint - checkpointDistances;
+
+    //calculates which checkpoint is on the other side of the train from the nearest checkpoint, works when turns are 90 degrees or less
+    float x, x1, y, y1, slope;
+    int secondClosestIndex;
+
+    x = lat;
+    y = lon;
+    x1 = redLineCheckpoints[closestIndex].lat;
+    y1 = redLineCheckpoints[closestIndex].lon;
+
+    if(redLineCheckpoints[closestIndex].lat > lat){
+      slope = (y1 - y) / (x1 - x);
+    }else{
+      slope = (y - y1) / (x - x1);
+    }
+
+    bool pointSide, nearestSide;
+    bool validTrain = true;
+    if(closestIndex == 0){
+      pointSide = (-1 / slope * (redLineCheckpoints[closestIndex + 1].lat - x) + y) > redLineCheckpoints[closestIndex + 1].lon;
+      nearestSide = (-1 / slope * (redLineCheckpoints[closestIndex].lat - x) + y) > redLineCheckpoints[closestIndex].lon;
+      if(pointSide == nearestSide){
+        validTrain = false;
+      }else{
+        secondClosestIndex = 1;
+      }
+    }else{
+      pointSide = (-1 / slope * (redLineCheckpoints[closestIndex - 1].lat - x) + y) > redLineCheckpoints[closestIndex - 1].lon;
+      nearestSide = (-1 / slope * (redLineCheckpoints[closestIndex].lat - x) + y) > redLineCheckpoints[closestIndex].lon;
+      if(closestIndex == checkpointCount - 1){
+        if(pointSide == nearestSide){
+          validTrain = false;
+        }else{
+          secondClosestIndex = checkpointCount - 2;
+        }
+      }else{
+        if(pointSide == nearestSide){
+          secondClosestIndex = closestIndex + 1;
+        }else{
+          secondClosestIndex = closestIndex - 1;
+        }
+      }
+    }
+
+    float segmentPos;
+    if(validTrain){
+      if(closestIndex < secondClosestIndex){
+        segmentPos = checkpointDistances[closestIndex] / (checkpointDistances[closestIndex] + checkpointDistances[secondClosestIndex]);
+        segmentPos *= redLineScalers[closestIndex];
+        for(int i = 0; i < closestIndex; i++){
+          segmentPos += redLineScalers[i];
+        }
+      }else{
+        segmentPos = checkpointDistances[secondClosestIndex] / (checkpointDistances[closestIndex] + checkpointDistances[secondClosestIndex]);
+        segmentPos *= redLineScalers[secondClosestIndex];
+        for(int i = 0; i < secondClosestIndex; i++){
+          segmentPos += redLineScalers[i];
+        }
+      }
+      redLineOutput[(int)floor(segmentPos)] = trainDir;
+      //Serial.printlnf("%i, %i, %f", closestIndex, secondClosestIndex, segmentPos);
+    }
 
     count++;
   }
+  for(int i = 0; i < RED_NUM_PIXELS; i++){
+    Serial.print(redLineOutput[i]);
+    strip.setPixelColor(i, 0);
+    if(redLineOutput[i] == 1){
+      strip.setPixelColor(i, 0xFF0000);
+    }else if(redLineOutput[i] == 5){
+      strip.setPixelColor(i, 0x0000FF);
+    }
+    redLineOutput[i] = 0;
+  }
+  strip.show();
+  Serial.println();
 
   // for(int i = 0; i < arraySize(redLineOutput); i++){
   //   if(redLineOutput[i] == 1){
@@ -134,6 +216,7 @@ void loop() {
 
   //strip.show();
   delay(5000);
+  Serial.println();
 }
 
 //clears up conflicts with multiple i2c slaves having the same address
