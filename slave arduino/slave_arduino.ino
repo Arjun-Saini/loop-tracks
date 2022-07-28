76ef81d0af7eaffcc32bc494fb11f59ed8a20f67
Ian Pratt <ianjdpratt@gmail.com>
2022-07-28 14:39:08 -0500

    aaa

--- slave arduino/slave_arduino.ino
+++ slave arduino/slave_arduino.ino
@@ -64,7 +64,6 @@ void loop()
     if (blink)
     {
         rainbow(5);
-        strip.show();
     }
 
     if (!verifyAddress)
@@ -130,6 +129,7 @@ void dataReceived(int count)
         }
         else if (inputBuffer[0] == '4')
         {
+            Serial.println("aufeh");
             blink = false;
             update = true;
             for (int i = 0; i < MAX_PIXELS; i++)
