58b4ca02507ada4e2da12e914f5861252bfd8412
Arjun-Saini <69866165+Arjun-Saini@users.noreply.github.com>
2022-08-15 12:39:15 -0500

    format

--- master/src/City.cpp
+++ master/src/City.cpp
@@ -1,14 +1,16 @@
 #include "Railway.cpp"
 
-class City{
-    public:
-        std::vector<Railway> railways;
-        std::string name;
-        int slaveCountExpected;
+class City
+{
+public:
+    std::vector<Railway> railways;
+    std::string name;
+    int slaveCountExpected;
 
-        City(std::vector<Railway> railwaysInput, std::string nameInput, int slaveCountExpectedInput){
-            railways = railwaysInput;
-            name = nameInput;
-            slaveCountExpected = slaveCountExpectedInput;
-        }
+    City(std::vector<Railway> railwaysInput, std::string nameInput, int slaveCountExpectedInput)
+    {
+        railways = railwaysInput;
+        name = nameInput;
+        slaveCountExpected = slaveCountExpectedInput;
+    }
 };
