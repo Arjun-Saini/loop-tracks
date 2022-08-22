24eb56590ce5cdddb98b1d7c46b58fdd631b2fd3
Arjun-Saini <69866165+Arjun-Saini@users.noreply.github.com>
2022-08-22 14:01:47 -0500

    repo cleanup

--- slave/target/1.5.2/argon/obj/src/user_module.o.d
+++
@@ -1,8 +0,0 @@
-e:/IoT/loop-tracks/slave/target/1.5.2/argon//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-
-../../../dynalib/inc/dynalib.h:
